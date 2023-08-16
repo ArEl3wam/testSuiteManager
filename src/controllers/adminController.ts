7e968c5bd60e2e6218f8e051b9da70632d124a28
Rana Gamal <ranaagamaall@gmail.com>
2023-08-16 14:36:29 +0300

    get all users, approve/delete user from admin

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,11 +1,50 @@
 import express from "express";
 import { getUserModel } from "../model/User";
+import { AppError } from "../shared/errors";
+const User = getUserModel();
+
 
 export async function getAllUnActivatedUsers(
   req: express.Request,
   res: express.Response
 ) {
-  const User = getUserModel();
   const users = await User.find({ isActive: false }).exec();
   res.status(200).json(users);
+}
+
+export async function getAllUsers(  
+  req: express.Request,
+  res: express.Response
+  ){
+    const activated = req.query.activated;
+    if(activated){
+      const users = await User.find({ isActive: true }).exec();
+      res.status(200).json(users);
+    }else{
+      const users = await User.find({ isActive: false }).exec();
+      res.status(200).json(users);
+    }
+}
+
+export async function activateUser(
+  req: express.Request,
+  res: express.Response
+){
+  const userId = req.params.id;
+  const approve = req.query.approve;
+  const user = await User.findById(userId);
+  if(!user) throw new AppError('This user does not exist.', 401);
+  try{
+    if(approve){
+      user.isActive = true;
+      await user.save();
+      return res.status(200).json(user);
+    }else{
+      await user.deleteOne();
+      return res.status(200).json({ message: 'User is deleted.' });
+    }
+  }catch(err:any){
+    return res.status(400).json({ message: err.message });
+  }
+
 }
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { isAdmin } from "../permissions/permissions";
-import { getAllUnActivatedUsers } from "../controllers/adminController";
+import { getAllUnActivatedUsers,  getAllUsers, activateUser} from "../controllers/adminController";
 
 export const adminRouter = express.Router();
 
 adminRouter.use(isAdmin);
 
 adminRouter.route("/admin/unactiveusers").get(getAllUnActivatedUsers);
+adminRouter.route("/admin/users").get(getAllUsers);
+adminRouter.route("/admin/users/:id").get(activateUser);
+
+
