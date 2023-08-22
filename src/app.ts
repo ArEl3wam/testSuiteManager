f3773941d26d5f54972e860062d00fe0d4b8c657
Ahmed Elawam <arelawam@gmail.com>
2023-08-22 13:09:26 +0300

    allowing requests with size up to 16 MB

--- src/app.ts
+++ src/app.ts
@@ -43,7 +43,7 @@ export function createApp() {
   };
   app.use(cors(corsOptions));
 
-  app.use(express.json({}));
+  app.use(express.json({limit: '16mb'}));
 
   app.use(cookieParser());
   app.use(authMiddleware); // this must be before swapDatabaseConnection
